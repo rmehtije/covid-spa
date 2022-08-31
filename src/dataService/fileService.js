111ba45e8ab91bcfa4ee8c871d52c26cb6923315
rmehtije <rmehtije@gmail.com>
2022-08-31 18:56:41 +0300

    url to json

--- src/dataService/fileService.js
+++ src/dataService/fileService.js
@@ -1,3 +1,3 @@
 export async function readCovidData() {
-    return await fetch('/owid-covid-data.json').then(res => res.json());
+    return await fetch('https://gammatest.net/course/javascript/react2/owid-covid-data.json').then(res => res.json());
 }
